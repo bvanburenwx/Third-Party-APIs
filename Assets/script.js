ea7d01abd9416a0d6bd0fec1ac851d9edda94ba3
BVanBuren <bvanburenwx@gmail.com>
2021-07-14 16:18:21 -0400

    Changed some of the styling on style.css. updated the script.js file to make one timeslot change colors based off past future present before working on other time slots. Changed index.html to add new selectors.

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,4 @@
+console.log(userHour);
 // Displaying current time at the top of the page.
 var today = moment().format("dddd LL");
 $("#currentDay").append(today);
@@ -5,16 +6,16 @@ $("#currentDay").append(today);
 var currentTime = moment().format("LT");
 $("#currentTime").append(currentTime);
 
-// add a listener to the save button
-$(".save-btn").on("click", function () {
+// add a listener to the save button and save time in local storage
+$(".saveBtn").on("click", function () {
     
-    console.log(this);
-    var text = $(this).siblings(".task").val();
-    var time = $(this).parent().attr("id");
+    var list = $(this).siblings(".task").val();
+    var timeStamp = $(this).parent().attr("id");
 
-    localStorage.setItem(time, text);
+    localStorage.setItem(timeStamp, list);
 })
 
+// Getting time from local storage to show time on screen upon refresh
 $("#8AM .task").val(localStorage.getItem("8AM"));
 $("#9AM .task").val(localStorage.getItem("9AM"));
 $("#10AM .task").val(localStorage.getItem("10AM"));
@@ -25,3 +26,25 @@ $("#2PM .task").val(localStorage.getItem("2PM"));
 $("#3PM .task").val(localStorage.getItem("3PM"));
 $("#4PM .task").val(localStorage.getItem("4PM"));
 $("#5PM .task").val(localStorage.getItem("5PM"));
+
+var hour8 = 8;
+var hour9 = 9;
+var hour10 = 10;
+var hour11 = 11;
+var hour12 = 12;
+var hour13 = 13;
+var hour14 = 14;
+var hour15 = 15;
+var hour16 = 16;
+var hour17 = 17;
+
+var userHour = moment().hour();
+console.log(userHour);
+
+if (userHour < hour10) {
+    $("#10AM-textarea").addClass("future");
+}   else if (userHour > hour10) {
+    $("#10AM-textarea").addClass("past");
+}   else if (userHour = hour10) {
+    $("#10AM-textarea").addClass("present");
+}

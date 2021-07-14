97dce4ce96144a87aa03c8c846131eb586685b80
BVanBuren <bvanburenwx@gmail.com>
2021-07-14 13:35:30 -0400

    got all local storage for each time slot to save and then bring back on the page upon refresh.

--- Assets/script.js
+++ Assets/script.js
@@ -16,3 +16,12 @@ $(".save-btn").on("click", function () {
 })
 
 $("#8AM .task").val(localStorage.getItem("8AM"));
+$("#9AM .task").val(localStorage.getItem("9AM"));
+$("#10AM .task").val(localStorage.getItem("10AM"));
+$("#11AM .task").val(localStorage.getItem("11AM"));
+$("#12PM .task").val(localStorage.getItem("12PM"));
+$("#1PM .task").val(localStorage.getItem("1PM"));
+$("#2PM .task").val(localStorage.getItem("2PM"));
+$("#3PM .task").val(localStorage.getItem("3PM"));
+$("#4PM .task").val(localStorage.getItem("4PM"));
+$("#5PM .task").val(localStorage.getItem("5PM"));
